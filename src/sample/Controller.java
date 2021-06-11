eec551d3a595f88595f8db22edf9c885555dbd84
CoolSheff131 <a.nikulsheev@gmail.com>
2021-06-11 20:57:46 +0800

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+    @FXML
+    private Button clientsBtn,workersBtn,bankBtn,settingBtn;
+
+
+
+    public void showBankMenu(ActionEvent actionEvent) {
+        System.out.println("Hello");
+    }
+
+    public void showSettingsMenu(ActionEvent actionEvent) {
+    }
+
+    public void showClientsMenu(ActionEvent actionEvent) {
+    }
+
+    public void showWorkersMenu(ActionEvent actionEvent) {
+    }
+}
